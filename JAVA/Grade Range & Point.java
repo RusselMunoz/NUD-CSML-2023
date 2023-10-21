3ade3a26f1007ec51b459f9795f2dd2ec2060de2
YokuDon <129136704+RusselMunoz@users.noreply.github.com>
2023-10-21 23:09:42 +0800

    Update Grade Range & Point.java

--- JAVA/Grade Range & Point.java
+++ JAVA/Grade Range & Point.java
@@ -13,26 +13,26 @@ public class assignment {
 
         double finalAverage = (midterm + finals) / 2;
         String gradePoint;
+		
+	int roundedFinalAverage = (int) finalAverage;
 
-        if (finalAverage >= 96 && finalAverage <= 100) {
+        if (roundedFinalAverage >= 96 && roundedFinalAverage <= 100) {
             gradePoint = "4.0";
-        } else if (finalAverage >= 90 && finalAverage <= 95) {
+        } else if (roundedFinalAverage >= 90 && roundedFinalAverage <= 95) {
             gradePoint = "3.5";
-        } else if (finalAverage >= 84 && finalAverage <= 89) {
+        } else if (roundedFinalAverage >= 84 && roundedFinalAverage <= 89) {
             gradePoint = "3.0";
-        } else if (finalAverage >= 78 && finalAverage <= 83) {
+        } else if (roundedFinalAverage >= 78 && roundedFinalAverage <= 83) {
             gradePoint = "2.5";
-        } else if (finalAverage >= 72 && finalAverage <= 77) {
+        } else if (roundedFinalAverage >= 72 && roundedFinalAverage <= 77) {
             gradePoint = "2.0";
-        } else if (finalAverage >= 66 && finalAverage <= 71) {
+        } else if (roundedFinalAverage >= 66 && roundedFinalAverage <= 71) {
             gradePoint = "1.5";
-        } else if (finalAverage >= 60 && finalAverage <= 65) {
+        } else if (roundedFinalAverage >= 60 && roundedFinalAverage <= 65) {
             gradePoint = "1.0";
         } else {
             gradePoint = "R";
         }
-
-        int roundedFinalAverage = (int) finalAverage;
 
         System.out.println("Final Avg: " + roundedFinalAverage);
         System.out.println("Grade point: " + gradePoint);
