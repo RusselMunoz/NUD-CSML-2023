6f9b268e70aab318df4edef0ee78ce882c34b75b
Russel Ron Muñoz <129136704+RusselMunoz@users.noreply.github.com>
2025-04-23 21:24:24 +0800

    Add files via upload

---
+++ sortalpabhet/sort.py
@@ -0,0 +1,5 @@
+item = ["trece martires", "palawan", "naic", "indang", "benguet", "gma", "imus", "alfonso", "dasmarinas", "bacoor", "gentri", "bacoor", "bulacan", "tagaytay", "laguna", "bongabong", "valenzuela", "muntinlupa", "pasig", "bansud", "taguig", "taytay", "mandaluyong", "dumaguete", "silay", "paranaque", "pampanga", "manila"]
+item.sort()
+print(item)
+
+item = ["silay", "dumaguete"]
